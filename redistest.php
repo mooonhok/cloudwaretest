4bfa977de0e1acfa78a27b84e78a31a7a3fa068c
mooonhok <1026413232@qq.com>
2018-04-04 13:34:16 +0800

    Update redistest.php

--- redistest.php
+++ redistest.php
@@ -6,16 +6,14 @@ require 'redisconfig.php';
 \Slim\Slim::registerAutoloader();
 $app = new \Slim\Slim();
 $arrays=array();
-$arrays['port']="127.0.0.1";
-$redis=new Redis($arrays);
+$redis=new Redis();
 
 $app->get('/alltenants',function()use($app,$redis) {
     $app->response->headers->set('Access-Control-Allow-Origin', '*');
     $app->response->headers->set('Content-Type', 'application/json');
-    $sales_id = $app->request->get("sales_id");
-   $redis->set('name',$sales_id);
+    $sales_id =1;
+    $redis->set('name',$sales_id);
     echo $redis->get('name');
-
 });
 
 
@@ -29,12 +27,4 @@ function file_url(){
     return files_url();
 }
 
-//加密
-function encode($string , $skey ) {
-    $strArr = str_split(base64_encode($string));
-    $strCount = count($strArr);
-    foreach (str_split($skey) as $key => $value)
-        $key < $strCount && $strArr[$key].=$value;
-    return str_replace(array('=', '+', '/'), array('O0O0O', 'o000o', 'oo00o'), join('', $strArr));
-}
 ?>
